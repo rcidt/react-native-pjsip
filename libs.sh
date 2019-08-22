3c23f49f94b6ce6b34f8d47e22b8441448c09ac8
Ricardo Corrie <rcorrie90@gmail.com>
2019-08-22 15:02:54 -0400

    Updates post install script

--- libs.sh
+++ libs.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 set -e
-rm -rf android
+
+if ! type "curl" > /dev/null; then 
+    echo "Missed curl dependency" >&2; 
+    exit 1; 
+fi
+if ! type "tar" > /dev/null; then 
+    echo "Missed tar dependency" >&2; 
+    exit 1; 
+fi
+
+curl -LO https://github.com/liveninja/react-native-pjsip/releases/download/1.0.0/VialerPJSIP.zip
+unzip VialerPJSIP.zip -d ios/VialerPJSIP.framework
+rm -rf VialerPJSIP.zip android
