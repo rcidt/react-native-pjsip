6a2784676e429af64d3c512e74882f0c9bb53d65
Ricardo Corrie <rcorrie90@gmail.com>
2020-08-20 15:27:55 -0400

    chore: [MOB-3007] Enable RTCP-XR for PjSIP

--- libs.sh
+++ libs.sh
@@ -10,6 +10,6 @@ if ! type "tar" > /dev/null; then
     exit 1; 
 fi
 
-curl -LO https://github.com/liveninja/react-native-pjsip/releases/download/1.0.0/VialerPJSIP.zip
+curl -LO https://github.com/liveninja/react-native-pjsip/releases/download/v1.2.0/VialerPJSIP.zip
 unzip VialerPJSIP.zip -d ios/VialerPJSIP.framework || true
 rm -rf VialerPJSIP.zip android || true
